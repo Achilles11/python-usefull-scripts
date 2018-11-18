bee50334cef82f076061ff9a92a78dcfdc252f61
Jakub Mertl <mertljakub@gmail.com>
2018-11-18 13:10:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This repository is full of usefull scritps for python
+
+Some text
---
+++ work_with_json.py
@@ -0,0 +1,44 @@
+import json
+
+
+def openCollection(path):
+    # Opening collection json
+    with open(path, 'r') as f:
+        postman_collection = json.load(f)
+
+    postman_collection_info = postman_collection["info"]
+
+    print("Name of collection is: " + str(postman_collection_info["name"]))
+
+    # Name of collection
+    postman_collection_items = postman_collection["item"]
+
+    # list of tests
+    for item in postman_collection_items:
+        print(item["name"])
+
+
+def openEnvironment(path):
+    # Opening environment json
+    with open(path, 'r') as f:
+        postman_environment = json.load(f)
+
+    # Name of environment
+    print("Name of environment is: " + postman_environment["name"])
+
+    for item in postman_environment["values"]:
+        print(item["key"] + " has value: " + item["value"])
+
+    # Change var3
+    for item in postman_environment["values"]:
+        if item["key"] == "Var3":
+            item["value"] = "Changed"
+
+    # Write out to tmp.json file
+    with open('tmp.json', 'w') as outfile:
+     json.dump(postman_environment, outfile, sort_keys = True, indent = 4,
+               ensure_ascii = False)
+
+
+#openCollection('data/postman_collections/box.json')
+openEnvironment('data/Test.postman_environment.json')
